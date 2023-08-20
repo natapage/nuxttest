461c96625b213ffeb802937e73b98a2abddf664d
natapage <624832@gmail.com>
2023-08-20 14:39:25 +0300

    test

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -2,12 +2,10 @@ import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import * as schema from "../db/schema";
 
-const db = drizzle(sql, {schema});
+const db = drizzle(sql, { schema });
 
 export default eventHandler(async (event) => {
-  const all = await db.query.users.findMany({})
-  const allUsers = await db.select().from(users);
-  const findById = await db.select().from(users).where({id: })
+  const allUsers = await db.query.users.findMany({});
   return {
     allUsers,
   };
